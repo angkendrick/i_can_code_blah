42a31e29f33e179f336bbdb54017ad12b7131190
Ken Ang <angkendrick@gmail.com>
2015-08-13 18:00:27 -0400

    completed tutorials CRUD

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -5,27 +5,52 @@ class TutorialsController < ApplicationController
   end
 
   def create
+    @tutorial = Tutorial.new(secure_params) #TODO: get user_id from user session
+
+    if @tutorial.save
+      redirect_to tutorials_path, notice: "Tutorial, #{@tutorial.name} submitted"
+    else
+      render :new
+    end
 
   end
 
   def new
-
+    @tutorial = Tutorial.new
   end
 
   def edit
-
+    @tutorial = Tutorial.find(params[:id])
   end
 
   def show
-
+    @tutorial = Tutorial.find(params[:id])
   end
 
   def update
+    @tutorial = Tutorial.find(params[:id])
 
+    if @tutorial.update_attributes(secure_params)
+      redirect_to tutorials_path, notice: "Tutorial, #{@tutorial.name} updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @tutorial = Tutorial.find(params[:id])
 
+    if @tutorial.destroy
+      redirect_to tutorials_path, notice: "Tutorial, #{@tutorial.name} deleted"
+    else
+      render :index
+    end
+  end
+
+  protected
+
+  def secure_params
+    params.require(:tutorial).permit(:user_id, :name, :description, :url, :image, :cost, :difficulty, :format, :company)
   end
 
 end
