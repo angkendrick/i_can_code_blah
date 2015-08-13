998f050283a5000f4a2b0fbb6d6c94abb8313db1
LaithAzer <laith.azer@gmail.com>
2015-08-13 17:31:56 +0000

    added more seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813161858) do
+ActiveRecord::Schema.define(version: 20150813165400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150813161858) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "tutorial_subjects", force: true do |t|
