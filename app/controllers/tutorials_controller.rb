0750e2b7e9b3b846682879896c42718ca46597d8
Ken Ang <angkendrick@gmail.com>
2015-08-13 16:25:45 -0400

    added index view

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,2 +1,31 @@
 class TutorialsController < ApplicationController
+
+  def index
+    @tutorials = Tutorial.all
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
