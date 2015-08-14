5d889e6432f3c053fc1485b43265f43cc89ea1cc
LaithAzer <laith.azer@gmail.com>
2015-08-14 20:29:26 +0000

    nothing done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :reviews
+  has_many :likes
 
   mount_uploader :avatar, AvatarUploader
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813231911) do
+ActiveRecord::Schema.define(version: 20150814202727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150813231911) do
   create_table "likes", force: true do |t|
     t.integer  "user_id"
     t.integer  "tutorial_id"
-    t.boolean  "completed"
+    t.boolean  "completed",   default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
