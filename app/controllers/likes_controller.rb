279c0220d65dea98a67f3fb29dd6863223d3a939
Ken Ang <angkendrick@gmail.com>
2015-08-14 21:23:46 -0400

    updating likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+
+  def index
+    @likes = Like.all
+  end
+
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @like = Like.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
   resources :search, only: [:index]
-  #get 'search', to: 'search#search'
+  resources :likes
 
   root to: 'tutorials#index'
   # The priority is based upon order of creation: first created -> highest priority.
